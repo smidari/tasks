be2a98564b29f224b54247761e839b8500374954
Dasha <smidariaa@gmail.com>
2020-06-24 16:54:55 +0300

    Made the first task

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Zadacha1 from "./components/Task1/Task1";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app-wrapper'>
+      <Zadacha1 name='Artem' text='npm start нажимал?' time='20:00'/>
     </div>
   );
 }
---
+++ src/components/Task1/Task1.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import style from "./Task1.module.css";
+import {Container, Row, Col, Image} from "react-bootstrap";
+
+type Task1Type = {
+    name: string,
+    text: string,
+    time: string
+}
+
+const Task1: React.FC<Task1Type> = props => (
+    <Container className={style.dialog}>
+        <Row>
+            <Col xs={4} md={1} className={style.avatar}>
+                <Image
+                    src="https://lh3.googleusercontent.com/proxy/ZPE6AtomrywrvAsG7RZ6u5RXGIdFjxV4Q_eNwE8BrpSe3zSTVOA66jkMxFXHoHJuXJ4ZfTHQ6S1BwKbkSYexFZ9ER-GaPcQZwr0KkDnRovSjEj2luIO0BqoM4Q"/>
+            </Col>
+            <Col xs={6} md={3} className={style.message}>
+                <h6>{props.name}</h6>
+                <span className={style.text}>{props.text}</span>
+                <span className={style.time}>{props.time}</span>
+            </Col>
+        </Row>
+    </Container>
+);
+
+export default Task1;
