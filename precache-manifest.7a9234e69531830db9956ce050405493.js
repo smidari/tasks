63ef43145152002703387607e7b18ccc038758a9
Dasha <smidariaa@gmail.com>
2020-07-22 10:09:40 +0300

    Updates

---
+++ precache-manifest.7a9234e69531830db9956ce050405493.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "117cb5b32fce0496744f9dceb4b37e34",
+    "url": "/tasks/index.html"
+  },
+  {
+    "revision": "2c6aa6445da11936ae11",
+    "url": "/tasks/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "64950171cdd18f5f18f9",
+    "url": "/tasks/static/css/main.ae7b8e33.chunk.css"
+  },
+  {
+    "revision": "2c6aa6445da11936ae11",
+    "url": "/tasks/static/js/2.eace3405.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/tasks/static/js/2.eace3405.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "64950171cdd18f5f18f9",
+    "url": "/tasks/static/js/main.e4540d2b.chunk.js"
+  },
+  {
+    "revision": "d85401926e6a56cee592",
+    "url": "/tasks/static/js/runtime-main.6f33b244.js"
+  }
+]);
